6f39044c95d865660caca744a0ec7755ca6cf026
Davronbek2811 <davronjon176@gmail.com>
2025-08-22 16:03:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let uzunlik = ["Olma","Banan", "Gilos", "Shaftoli"];
+let alart = confirm("Boshlanishida array uzunliki; 4 ");
+console.log(uzunlik)
+let alrt1 = confirm("Sizning array malumotingizni olib tashamoqchimisiz ?");
+
+
+if(alrt1){
+    uzunlik.splice(3,1)
+}
